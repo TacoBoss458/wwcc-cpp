734739196da8f7e142380e32a419025b07e58ecd
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-12 12:10:11 -0700

    Added new features

--- assignment3.cpp
+++ assignment3.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <string>
+using namespace std;
 int main(){
-    std::string answer;
+    string answer;
     float temp;
     double Fahrenheit = (temp * 9.0/5.0) + 32.0;
     double Celsius = (temp - 32.0) * 5.0/9.0;
-    std::cout << "Enter a tempreture. " << std::endl;
-    std::cin >> temp;
-    std::cout << "Enter 'c' for Celsius or 'f' for Fahrenheit. ";
-    std::cin >> answer;
+    cout << "Enter a tempreture. " << endl;
+    cin >> temp;
+    cout << "Enter 'c' for Celsius or 'f' for Fahrenheit. " << endl;
+    cin >> answer;
     if (answer == "c")
     {
-    std::cout << "Your tempreture of " << temp << " degrees Celsius is equivilent to " << Fahrenheit << " degrees Fahrenhiet.";
+    cout << "Your tempreture of " << temp << " degrees Celsius is equivilent to " << Fahrenheit << " degrees Fahrenhiet." << endl;
     } else if (answer == "f"){
-        std::cout << "Your tempreture of " << temp << " degrees Fahrenhiet is equivalent to " << Celsius << " degrees Celsius.";
+        cout << "Your tempreture of " << temp << " degrees Fahrenhiet is equivalent to " << Celsius << " degrees Celsius."<< endl;
     }
     return 0;
     
