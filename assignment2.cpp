2712da957d1b4e74e262ea413df44c83abd9e4ff
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-12 13:43:35 -0700

    added new features

--- assignment2.cpp
+++ assignment2.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include <string>
-int main () {
-    std::string num1, num2;
-    std::cout << "Enter two numbers: " << std::endl;
-    std::cin >> num1 >> num2;
+#include <string> 
+using namespace std;
+int main (){
+    int num1;
+    double num2;
+    double quotient = num1 / num2;
+    cout << "Enter a number: " << endl;
+    cin >> num1;
+    cout << "Enter another number: " << endl;
+    cin >> num2;
+    cout << "Sum: " << num1 + num2 << endl;
+    cout << "Difference: " << num1 - num2 << endl;
+    cout << "Product: " << num1 * num2 << endl;
+    if (num2 = 0){
+        cout << "Divided by 0, UNDEFINED." << endl;
+    }
+    else {
+        cout << "Quotient: " << quotient << endl;
+    }
+    return 0;
 }
