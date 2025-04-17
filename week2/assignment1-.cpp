5e85d2e7ba8be40ef578b0b93f5f7469bb4e63fa
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-17 16:38:39 -0700

    Added new features

--- week2/assignment1-.cpp
+++ week2/assignment1-.cpp
@@ -3,27 +3,23 @@
 #include <ctime>    // For time() function
 using namespace std;
 
-
+//sets the range from 0-1
 bool flipCoin() {
     return rand() % 2;
     }
-    
-
-
 int main() {
-    // Seed the random number generator
-    srand(time(0));
 
+    srand(time(0));
+// vars
     int flips;
     int heads = 0;
     int tails = 0;
-
-
-
+//intro
     cout << "Coin Flip Simulator" << endl;
     cout << "How many flips would you like to simulate? ";
     cin >> flips;
     cout << "Simulating " << flips << " coin flips... \n\nResults:\n";
+//where the coin actually flips
     for (int i = 0; i < flips; i++){
         if (flipCoin()) {
             ++heads;
@@ -32,13 +28,10 @@ int main() {
         }
         
     }
+//calculators
     double hPer = (static_cast <double> (heads) / flips) * 100;
     double tPer = (static_cast <double> (tails) / flips) * 100;    
     cout << "Coin landed on Heads " << heads << " times (" << hPer << "%)." << endl;
     cout << "Coin landed on Tails " << tails << " times (" << tPer << "%)." << endl;
-    
-    
-
-
     return 0;
 }
