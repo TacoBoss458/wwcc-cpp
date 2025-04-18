844f31559ddb94fb1efe8f485b6929bd974f2a42
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-17 19:05:40 -0700

    Added new features

--- week2/assignment2-.cpp
+++ week2/assignment2-.cpp
@@ -15,18 +15,22 @@ int main() {
     cout << "Number Guessing Game" << endl;
     cout << "I'm thinking of a number between 1 and 100." << endl << endl;
     while (guess !=secretNumber){
+        cout << "Take a guess..." << endl;
+        cin >> guess;
         if (guess < secretNumber){
-            cout << "Too low. Try again." <<endl;
-            ++guess;
+            cout << "Too low. Try again." << endl << endl;
+            ++attempts;
             continue;
         }
         else if (guess > secretNumber){
-            cout << "Too high. Try again." << endl;
-            ++guess;
+            cout << "Too high. Try again." << endl << endl;
+            ++attempts;
             continue;
         }
         else{
+            ++attempts;
             cout << "Congratulations! You guessed the number!" << endl;
+            cout << "It only took you " << attempts << " guesses!" << endl;
         }
     }
     //what needs to happen is it needs to guess a number,
