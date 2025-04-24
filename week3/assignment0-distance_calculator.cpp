bf69b7145b1c6df34b920dfb5de78a35b41dd062
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-23 17:47:23 -0700

    Added new features

---
+++ week3/assignment0-distance_calculator.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+int main(){
+    int num1 = 7;
+    int num2 = 4;
+
+    int addnumbers(int num1, int num2) {
+        int sum = num1 + num2;
+    }
+    int result = addnumbers;
+}
