cf828c01a3cf9288204e596567d531dd655c8483
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-23 19:10:42 -0700

    Added new changes

--- week3/assignment0-distance_calculator.cpp
+++ week3/assignment0-distance_calculator.cpp
@@ -1,16 +1,17 @@
 #include <iostream> 
 #include <cmath>
 using namespace std;
-double x1;
-double x2;
-double y1;
-double y2;
 
-int calculateDistance(double x1, double x2, double y1, double y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+double calculateDistance(double X1_param, double X2_param, double Y1_param, double Y2_param) {
+    return sqrt(pow(X2_param - X1_param, 2) + pow(Y2_param - Y1_param, 2));
 }
 int main () {
-    cout << "Enter two points on a 2D Cartesian coordinate plane. \n\nPoint one\nX1: " ;
+    double x1;
+    double x2;
+    double y1;
+    double y2;
+    cout << "\nEnter two points on a 2D Cartesian coordinate plane. \n\nPoint one\nX1: " ;
     cin >> x1;
     cout << "Y1: ";
     cin >> y1;
@@ -18,8 +19,9 @@ int main () {
     cin >> x2;
     cout << "Y2: ";
     cin >> y2;
-    double result = calculateDistance(x1, y1, x2, y2);
+    double result = calculateDistance(x1, x2, y1, y2);
     cout << "The distance between (" << x1 << "," << y1 << ") and (" 
-    << x2 << "," << y2 << ") is " << result << "!";
+    << x2 << "," << y2 << ") is " << result << "!" << endl;
+    return 0;
 
 }
