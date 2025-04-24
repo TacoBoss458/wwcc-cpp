91b0e078bc28762877f88a5f0ad967659edd227e
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-23 18:51:51 -0700

    Added new features

--- week3/assignment0-distance_calculator.cpp
+++ week3/assignment0-distance_calculator.cpp
@@ -1,11 +1,25 @@
 #include <iostream> 
-int main(){
-    int num1 = 7;
-    int num2 = 4;
+#include <cmath>
+using namespace std;
+double x1;
+double x2;
+double y1;
+double y2;
 
-    int addnumbers(int num1, int num2) {
-        int sum = num1 + num2;
-    }
-    int result = addnumbers;
-    return 0;
+int calculateDistance(double x1, double x2, double y1, double y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+int main () {
+    cout << "Enter two points on a 2D Cartesian coordinate plane. \n\nPoint one\nX1: " ;
+    cin >> x1;
+    cout << "Y1: ";
+    cin >> y1;
+    cout << "Point two\nX2: ";
+    cin >> x2;
+    cout << "Y2: ";
+    cin >> y2;
+    double result = calculateDistance(x1, y1, x2, y2);
+    cout << "The distance between (" << x1 << "," << y1 << ") and (" 
+    << x2 << "," << y2 << ") is " << result << "!";
+
 }
