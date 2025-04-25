0e5e66630a6fec96a6534aa4194ddf6e988077bd
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-24 21:14:40 -0700

    Added new features

---
+++ week3/assignment1-print_formatter.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+int format(int answerA) {
+    ;
+}
+double format(double double_d2) {
+    return ;
+}
+double format(double double_d0) {
+    return ;
+}
+double format(double double_d2) {
+    return ;
+}
+
+
+int main() {
+    int answerA;
+    double answerB, answerC;
+    string answerD;
+    cout << "\nWelcome to the Print Formatter!\n";
+    cout << "\nEnter an Integer into the Print Formatter." << endl;
+    cin >> answerA;
+    cout << "Original: " << answerA << "\nFormatted: " <<regular() ;
+    cout << "\nEnter a Number into the Print Formatter.(Formatted to 4 decimal places)" << endl;
+    cin >> answerB;
+    cout << "";
+    cout << "\nEnter a Number into the Print Formatter.(Formatted to 2 decimal places)" << endl;
+    cin >> answerC;
+    cout << "";
+    cout << "\nEnter a String into the Print Formatter." << endl;
+    cin >> answerD;
+    
+}
+}
+}
+
