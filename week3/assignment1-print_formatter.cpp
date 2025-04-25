28c54382c9d5c265c5148d81eaa98fc8bc9ad56a
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-04-24 23:14:37 -0700

    Added new features

--- week3/assignment1-print_formatter.cpp
+++ week3/assignment1-print_formatter.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+#include <string>
+#include <cctype>
 using namespace std;
 
-int format(int answerA) {
-    ;
+// Function to format number with commas
+string formatWithCommas(int number) { 
+    string numStr = to_string(number); //int to string
+    int insertPosition = numStr.length() - 3; //initial comma
+    while (insertPosition > 0) {
+        numStr.insert(insertPosition, ",");
+        insertPosition -= 3;
+    }//every 3 didgits is a comma
+    return numStr;//should return the formmatted string
 }
-double format(double double_d2) {
-    return ;
+string formatWithPrecisionTwo(double number2) {
+    ostringstream stream;
+    stream << fixed << setprecision(2) << number2;
+    return stream.str();
 }
-double format(double double_d0) {
-    return ;
+
+string formatWithPrecisionFour(double number3, int precision) {
+    ostringstream out;
+    out << fixed << setprecision(precision) << number3;
+    return out.str();
 }
-double format(double double_d2) {
-    return ;
-}
+//??
+
+
+
+
 
 
 int main() {
-    int answerA;
-    double answerB, answerC;
-    string answerD;
-    cout << "\nWelcome to the Print Formatter!\n";
-    cout << "\nEnter an Integer into the Print Formatter." << endl;
-    cin >> answerA;
-    cout << "Original: " << answerA << "\nFormatted: " <<regular() ;
-    cout << "\nEnter a Number into the Print Formatter.(Formatted to 4 decimal places)" << endl;
-    cin >> answerB;
-    cout << "";
-    cout << "\nEnter a Number into the Print Formatter.(Formatted to 2 decimal places)" << endl;
-    cin >> answerC;
-    cout << "";
-    cout << "\nEnter a String into the Print Formatter." << endl;
-    cin >> answerD;
-    
-}
-}
-}
+    int commas = 1234567;
+    double precision = 1000.995;
+    double preFour = 23456.77;
+    int precisionFour = 4;
+    string caps;
 
+    string formatted1 = formatWithCommas(commas);
+    cout << "\nPrint Formatter\n\nOriginal: 1234567\nFormatted: "<< formatted1;
+    string formatted2 = formatWithPrecisionTwo(precision);
+    cout << "\nOriginal: 1000.995\nFormatted to 2 decimal places: "<< formatted2;
+    string formatted3 = formatWithPrecisionFour(preFour, precisionFour);
+    cout << "\nOriginal: 23456.77\nFormatted with 4 decimal places: "<< formatted3 <<endl << endl;
+}
