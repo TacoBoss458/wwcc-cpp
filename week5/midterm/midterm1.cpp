4507624169aff6d339a8ac0dd45bd7a1cc99518b
TacoBoss458 <isaiah.a.rusch@student.wwcc.edu>
2025-05-12 14:31:58 -0700

    Added new features

---
+++ week5/midterm/midterm1.cpp
@@ -0,0 +1,6 @@
+// Isaiah Rusch
+// This midterm covers project 7. I want this program to simulate a menu style program that
+// sorts the menu between a movie adder, a movie searcher, year filter, a statistic display, 
+// a watchlist area, and finally an exit prompt. It needs to allow the user to enter as many movies
+// as they want. This means that each of the menu prompts needs to be infinite until the user 
+// prompts to extt. that also means that there should be an exit prompt after each menu and sub menu.
